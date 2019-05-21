9a1b83116f310f16c990868cec8d0b26bbe0a11a
Alex Beznos <beznosa@yahoo.com>
2019-05-21 21:46:37 +0300

    Fix

--- routes/ping.rb
+++ routes/ping.rb
@@ -1,7 +1,12 @@
 class StreamingExample
   route "ping" do |r|
     r.get do
+      require 'pry'; binding.pry
       r.halt 200, "pong"
+    end
+    r.post do
+      require 'pry'; binding.pry
+      r.halt 200
     end
   end
 end
